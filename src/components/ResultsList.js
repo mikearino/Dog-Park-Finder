275af025f393cc5ad682bd2450aaa24e0700b738
Mike Lofgren <mikeylofgren@gmail.com>
2019-12-28 23:04:01 -0800

    render flatlist.

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -5,7 +5,14 @@ const ResultsList = ({ title, results }) => {
   return (
     <View>
       <Text style={styles.title}>{title}</Text>
-      <Text>Results: {results.length}</Text>
+      <FlatList
+        horizontal
+        data={results}
+        keyExtractor={result => result.id}
+        renderItem={({ item }) => {
+          return <Text>{item.name}</Text>;
+        }}
+      />
     </View>
   );
 };
