500c71f37fc79ce787c8d3cbea707c088c4b7a11
Mike Lofgren <mikeylofgren@gmail.com>
2019-12-28 22:58:07 -0800

    filter results.

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -8,6 +8,14 @@ const SearchScreen = () => {
   const [keyword, setKeyword] = useState("");
   const [searchApi, results, errorMessage] = useResults();
 
+  const filterResultsByRating = rating => {
+    return results.filter(result => {
+      // return rating <= result.rating ? result.rating : null;
+      return result.rating === rating;
+    });
+  };
+  console.log(results);
+
   return (
     <View>
       <SearchBar
@@ -17,9 +25,9 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
-      <ResultsList title="Great Dog Park" />
-      <ResultsList title="Good Dog Park" />
-      <ResultsList title="Skip It" />
+      <ResultsList results={filterResultsByRating(5)} title="Great Dog Park" />
+      <ResultsList results={filterResultsByRating(4)} title="Good Dog Park" />
+      <ResultsList results={filterResultsByRating(3.5)} title="Skip It" />
     </View>
   );
 };
