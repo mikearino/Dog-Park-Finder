a00aebdb0240ac3bfbb6d66f638856e3397add29
Mike Lofgren <mikeylofgren@gmail.com>
2019-12-28 23:17:22 -0800

    work on styles.

---
+++ src/components/ResultsDetail.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { View, Image, Text, StyleSheet } from "react-native";
+
+const ResultsDetail = ({ result }) => {
+  return (
+    <View>
+      <Image style={styles.image} source={{ uri: result.image_url }} />
+      <Text style={styles.name}>{result.name}</Text>
+      <Text>
+        {result.rating} Stars, {result.review_count} Reviews
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  image: {
+    width: 250,
+    height: 120,
+    borderRadius: 5
+  },
+  name: {
+    fontWeight: "bold"
+  }
+});
+
+export default ResultsDetail;
