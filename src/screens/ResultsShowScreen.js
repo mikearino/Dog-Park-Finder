09869fca5b4d4c4fb0384c23b960e58a48625988
Mike Lofgren <mikeylofgren@gmail.com>
2019-12-29 00:11:46 -0800

    add another api call to individual screens.

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import yelp from "../api/yelp";
 
-const ResultsShowScreen = () => {
+const ResultsShowScreen = ({ navigation }) => {
+  const [result, setResult] = useState(null);
+  const id = navigation.getParam("id");
+  console.log(result);
+  const getResult = async id => {
+    const response = await yelp.get(`${id}`);
+    setResult(response.data);
+  };
+  useEffect(() => {
+    getResult(id);
+  }, []);
+
   return (
     <View>
       <Text>Results Show</Text>
